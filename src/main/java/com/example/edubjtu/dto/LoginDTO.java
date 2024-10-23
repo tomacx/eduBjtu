b0012aabcd4cdfcfba9cb1af319c59204f45a78e
tomacx <139841470+tomacx@users.noreply.github.com>
2024-10-23 12:51:14 +0800

    dto通用类

--- src/main/java/com/example/edubjtu/dto/LoginDTO.java
+++ src/main/java/com/example/edubjtu/dto/LoginDTO.java
@@ -1,23 +1,9 @@
 package com.example.edubjtu.dto;
 
-public class LoginDTO {
+import lombok.Data;
+
+@Data
+public class LoginDto {
     private String username;
     private String password;
-
-    // Getters and setters
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
